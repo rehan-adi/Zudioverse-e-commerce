4894058f97778d14c96422a07ab074d5138aa560
Rehan <rehanalire52@gmail.com>
2024-01-06 20:28:33 +0530

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,21 +2,42 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/pagination';
+// import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import './CartProduct/swiper.css';
 
 function About() {
   return (
-    <div id='about' className='w-auto flex justify-center pt-16 items-center flex-col h-[150vh] bg-white'>
+    <div id='about' className='w-auto flex justify-center pt-28 items-center flex-col h-[260vh] bg-white'>
       <h1 className='text-4xl font-semibold'>About ZUDIOVERSE</h1>
       <p className='text-center line mt-6'>We are a lifestyle retailer dedicated to inspiring customers through a unique <br /> combination of product, creativity and cultural understanding. Founded in 1970 in a <br /> small space across the street from the University of Pennsylvania.</p>
       <p className='text-center line mt-6'> <span className='text-black font-semibold'>ZUDIOVERSE</span> now operates over 200 stores in the United States, Canada and Europe, <br /> offering experiential retail environments and a well-curated mix men's product <br /> assortments.</p>
-      <p className='mt-14 text-xl font-semibold opacity-85'>OUR STORY</p>
+      <div className='flex gap-96 pb-9 pt-24 items-center'>
+        <div className=''>
+          <h1 className='text-5xl mad font-semibold'>WE EMPOWER YOU TO <br /> MOVE COMFORTABLY</h1>
+          <p className='text-lg mt-7 opacity-70'>At Athletics, we're about creating products that <br /> amplify your workouts. Lorem ipsum dol amet, <br /> consectetur piscis rabitur cursus lectus por.</p>
+          <button className='py-3 px-4 mt-8 bg-black hover:opacity-80 text-white'>Shop Now</button>
+        </div>
+        <div>
+          <img src="https://assets.website-files.com/61983c3a41be5bad7444a3c0/61f2e70177f8584393e77273_Image020%201.jpg" className='rotate-6' width={180} alt="" />
+        </div>
+      </div>
+      <div className='flex gap-28 mt-8 mb-5 items-center'>
+        <div>
+          <img src="https://assets.website-files.com/61983c3a41be5bad7444a3c0/61f2e7539d538babf0f423db_Image021%201.jpg" className='' width={480} alt="" />
+        </div>
+        <div>
+          <h3 className='mb-5 text-lg opacity-60'>OUR FOUNDER</h3>
+          <h1 className='text-5xl mad font-semibold'>BEAUTY <br /> BEHIND <br />
+            THE MADNESS</h1>
+            <p className='text-lg mt-7 opacity-70'>Rehan started this company in his room <br /> apartment. Rabitur cursus ut lectus porta <br /> placerat. Vamus quaise facilisis nulla id dui <br /> ague sellus portor.</p>
+        </div>
+      </div>
+      {/* <p className='mt-14 text-xl font-semibold opacity-85'>OUR STORY</p>
       <h1 className='text-4xl mt-3 font-semibold'>STARTED FROM
         THE BOTTOM</h1>
       <p className='mt-6 text-center'>Now we're here. Consectetur piscis elit Rabitur cursus ut lectus porta placerat. Vivamus quis facilisis <br /> nulla id dui augue sellus porttitor. Fabrics and gear made for every workout. Find the right athletic <br /> wear to reach new goals in comfort. Our innovative fabric gives you the comfort to flow through <br /> your goals in every movement and find a better work.</p>
-      <p className='text-center mb-5 mt-3'>we're about creating products that amplify your workouts. Lorem ipsum dol amet, consectetur <br /> piscis rabitur cursus lectus por.</p>
+      <p className='text-center mb-5 mt-3'>we're about creating products that amplify your workouts. Lorem ipsum dol amet, consectetur <br /> piscis rabitur cursus lectus por.</p> */}
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
