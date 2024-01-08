e633ab844bbc0f8f56b8a29626049cfcd130ac57
Rehan <rehanalire52@gmail.com>
2024-01-08 22:56:29 +0530

    Strip Update

--- src/Components/CartProduct/Tshirt40s.jsx
+++ src/Components/CartProduct/Tshirt40s.jsx
@@ -44,7 +44,9 @@ function Tshirt40s() {
         <input type="number" className='mt-3 w-16 border pl-4 py-2 ' placeholder='1' />
         <div>
         <button className='py-3 mr-6 px-4 mt-8 bg-black hover:opacity-80 text-white' onClick={() => Submit(item)}>Add to Cart</button>
+        <a href="https://buy.stripe.com/test_7sI6os1iJ8O4ggweUV">
         <button className='py-3 px-4 bg-white border font-semibold border-black text-center text-black'>Shop<span><TiShoppingCart className='inline-block text-xl ml-2 ' /></span></button>
+        </a>
         <h1 className='mt-5 font-semibold'> <ImTruck  className='inline-block mr-2'/>Free shipping & returns in all orders.</h1>
         </div>
     </div>
