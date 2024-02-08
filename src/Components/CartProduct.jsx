2ec0de82fba98d05d71a7f53c88351718dffeb4b
Rehan <rehanalire52@gmail.com>
2024-02-08 22:42:44 +0530

    Resposive update on CartProducts

--- src/Components/CartProduct.jsx
+++ src/Components/CartProduct.jsx
@@ -16,36 +16,35 @@ function CartProduct() {
   };
 
   return (
-    <div className='lg:h-[140vh] h-[100vh] w-auto flex flex-col justify-start pt-28 items-center text-black bg-white '>
-         <ToastContainer />
-      <h1 className='lg:text-4xl text-3xl mb-16 font-semibold'>Your Cart Section</h1>
-      
+    <div className='w-full lg:w-auto lg:max-w-screen-lg mx-auto min-h-screen flex flex-col justify-start pt-28 items-center text-black bg-white'>
+      <ToastContainer />
+      <h1 className='lg:text-4xl text-3xl mb-8 font-semibold'>Your Cart Section</h1>
+
       {cartItems.length === 0 ? (
         <div className='flex flex-col items-center'>
-          <p className='font-semibold mb-16 text-2xl'>Your cart is empty</p>
-          <FiShoppingCart className='text-[200px] lg:mr-0 mr-6' />
+          <p className='font-semibold mb-6 text-xl'>Your cart is empty</p>
+          <FiShoppingCart className='text-[100px] mb-6' />
           <Link to="/Products">
-          <button className='bg-black ml-10 mt-8 mr-4 lg:mr-0 text-white px-9 py-3'>Shop</button>
+            <button className='bg-black text-white lg:ml-0 ml-5 px-6 py-2'>Shop</button>
           </Link>
         </div>
       ) : (
-        <ul className='flex justify-center mt-20 flex-col gap-3 w-[80vw] items-center h-[220px]'>
+        <ul className='lg:flex justify-center text-center items-center lg:mt-20 mt-10 px-10 lg:px-0 lg:flex-col lg:gap-3 w-full'>
           {cartItems.map((item) => (
-            <li className='flex border-2 items-center' key={item.id}>
+            <li className='lg:flex border-2 lg:py-0 py-4 items-center mb-6' key={item.id}>
               <img
                 src={item.img}
                 alt={item.name}
-                className='mr-4'
-                style={{ width: '170px', height: '210px', marginLeft: '25px' }}
+                className='lg:mr-4  w-full'
+                style={{ maxWidth: '160px', height: '200px', marginLeft: '65px' }}
               />
-              <div className='flex items-center px-[50px] gap-32'>
-                <p className='font-semibold'>{item.name}</p>
-                <p className='font-semibold'>
+              <div className='lg:flex items-center lg:mt-0 mt-8 lg:px-[50px] lg:gap-32 w-full'>
+                <p className='font-semibold lg:mb-0 mb-3'>{item.name}</p>
+                <p className='font-semibold lg:mb-0 mb-3'>
                   <span className='opacity-55 line-through'>$55.80</span> ${item.price}
                 </p>
-                <p className='font-semibold'>Quantity: {item.quantity}</p>
+                <p className='font-semibold lg:mb-0 mb-4 '>Quantity: {item.quantity}</p>
                 <button className='bg-black text-white px-5 py-3' onClick={() => { notify(); removeItemHandler(item); }}>Remove</button>
-
               </div>
             </li>
           ))}
