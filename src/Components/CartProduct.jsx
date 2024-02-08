3ebc8092bc7c0fa1d84b88393db982e0d4ea414c
Rehan <rehanalire52@gmail.com>
2024-02-08 22:51:34 +0530

    More Updates

--- src/Components/CartProduct.jsx
+++ src/Components/CartProduct.jsx
@@ -21,15 +21,15 @@ function CartProduct() {
       <h1 className='lg:text-4xl text-3xl mb-8 font-semibold'>Your Cart Section</h1>
 
       {cartItems.length === 0 ? (
-        <div className='flex flex-col items-center'>
-          <p className='font-semibold mb-6 text-xl'>Your cart is empty</p>
-          <FiShoppingCart className='text-[100px] mb-6' />
+        <div className='flex flex-col mt-10 items-center'>
+          <p className='font-semibold mb-6 lg:text-3xl text-xl'>Your cart is empty</p>
+          <FiShoppingCart className='text-[120px] lg:text-[210px] mb-7' />
           <Link to="/Products">
-            <button className='bg-black text-white lg:ml-0 ml-5 px-6 py-2'>Shop</button>
+            <button className='bg-black text-white lg:ml-12 ml-5 lg:px-10 px-6 py-2'>Shop</button>
           </Link>
         </div>
       ) : (
-        <ul className='lg:flex justify-center text-center items-center lg:mt-20 mt-10 px-10 lg:px-0 lg:flex-col lg:gap-3 w-full'>
+        <ul className='lg:flex justify-center text-center items-center lg:mt-14 mt-10 px-10 lg:px-0 lg:flex-col lg:gap-3 w-full'>
           {cartItems.map((item) => (
             <li className='lg:flex border-2 lg:py-0 py-4 items-center mb-6' key={item.id}>
               <img
@@ -39,11 +39,11 @@ function CartProduct() {
                 style={{ maxWidth: '160px', height: '200px', marginLeft: '65px' }}
               />
               <div className='lg:flex items-center lg:mt-0 mt-8 lg:px-[50px] lg:gap-32 w-full'>
-                <p className='font-semibold lg:mb-0 mb-3'>{item.name}</p>
-                <p className='font-semibold lg:mb-0 mb-3'>
+                <p className='font-semibold lg:mb-0 mb-4'>{item.name}</p>
+                <p className='font-semibold lg:mb-0 mb-4'>
                   <span className='opacity-55 line-through'>$55.80</span> ${item.price}
                 </p>
-                <p className='font-semibold lg:mb-0 mb-4 '>Quantity: {item.quantity}</p>
+                <p className='font-semibold lg:mb-0 mb-6 '>Quantity: {item.quantity}</p>
                 <button className='bg-black text-white px-5 py-3' onClick={() => { notify(); removeItemHandler(item); }}>Remove</button>
               </div>
             </li>
