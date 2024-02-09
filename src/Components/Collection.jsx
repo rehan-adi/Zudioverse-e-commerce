6fc6dc79b2981b8fbf7209d1ca3e76ff0c4f62cb
Rehan <rehanalire52@gmail.com>
2024-02-09 12:12:04 +0530

    Update in collection page

--- src/Components/Collection.jsx
+++ src/Components/Collection.jsx
@@ -92,7 +92,7 @@ function Collection() {
 
   return (
     <div className='max-h-fit w-auto bg-white'>
-          <div id='home' className='w-auto flex justify-center flex-col lg:flex-row gap-28 md:gap-32 lg:gap-40 overflow-hidden items-center h-[180vh]  md:h-[180vh] lg:h-screen bg-white'>
+          <div id='home' className='w-auto flex justify-center flex-col lg:flex-row gap-28 md:gap-32 lg:gap-40 overflow-hidden items-center max-h-fit pt-20 pb-16  md:h-[180vh] lg:h-screen bg-white'>
              <div>
                   <motion.h4 
                   initial={{opacity: 0, y : 30, rotate: 10}}
@@ -142,13 +142,15 @@ function Collection() {
                 <img src="https://uploads-ssl.webflow.com/5c7b4529d99ee5d38a884f51/5c7c6df7d99ee57ba589af62_beanie-casual-fashion-1967565%20(1).jpg" className=' md:w-[575px] lg:w-[650px]'  width={330}  alt="img" />
              </div>
     </div>
+      <div className='flex justify-center lg:justify-start lg:ml-[70px] items-center'>
       <motion.h1
        initial={{opacity: 0, x : -35}}
        animate={controls1}
        ref={ref}  
-      className='lg:text-5xl text-2xl mt-0 lg:mt-24 md:ml-24 ml-6 mb-8 lg:mb-8 md:text-4xl lg:ml-[70px] font-bold'>
+      className='lg:text-5xl text-2xl mt-0 lg:mt-24  mb-8 lg:mb-8 md:text-4xl font-bold'>
         Curated <span className='opacity-55 text-[#333333]'>Collections</span>
       </motion.h1>
+      </div>
       <div className='flex justify-center flex-col lg:flex-row mt-5 gap-10 items-center'>
         <div className='relative'>
           <img src="https://oqium.com/cdn/shop/files/IMG_2512_2.jpg?v=1695800703&width=430" className='lg:w-[430px] md:w-[575px] ' width={320} alt="" />
@@ -165,17 +167,27 @@ function Collection() {
           <p style={imageTextStyle} className='uppercase' >Less Impact</p>
         </div>
       </div>
-      <div>
+      <div className='flex justify-center lg:items-start items-center flex-col'>
           <motion.h1
            initial={{opacity: 0, x : -35}}
            animate={weekend}
            ref={ref1}   
-          className='lg:text-5xl text-2xl md:text-4xl md:mb-12 md:ml-24 mt-14 ml-7 lg:ml-[70px] font-bold'>Featured <span className='opacity-55 text-[#333333]'>Categories</span></motion.h1>
+          className='lg:text-5xl text-2xl md:text-4xl md:mb-12 mt-14 lg:ml-[70px] font-bold'>Featured <span className='opacity-55 text-[#333333]'>Categories</span></motion.h1>
           <motion.p
            initial={{opacity: 0, x : -35}}
            animate={weekend1}
            ref={ref}   
-          className='lg:text-xl text-sm font-semibold md:ml-24 md:text-2xl ml-7 lg:ml-[70px] mt-5 '>All weather condition - Jacket - T-Shirt - Shirt - Shorts and many more. </motion.p>
+          className='lg:text-xl text-sm font-semibold hidden lg:flex md:text-2xl lg:ml-[70px] mt-5 '>
+            All weather condition - Jacket - T-Shirt - Shirt - Shorts and many more.
+            </motion.p>
+            <motion.p
+            initial={{opacity: 0, x : -35}}
+            animate={weekend1}
+            ref={ref}   
+            className='lg:text-xl lg:hidden flex text-sm font-semibold md:text-2xl lg:ml-[70px] mt-5'
+            >
+            Jacket - T-Shirt - Shirt - Shorts
+            </motion.p>
       </div>
       <div className='w-auto h-[16vh] flex justify-center mt-16 items-center bg-yellow-500'>
             <h1 className='font-semibold uppercase text-xs md:text-2xl lg:text-2xl'>ZudioVerse Offer <span> 
@@ -194,23 +206,23 @@ function Collection() {
                 </span>
             </h1>
     </div>
-    <div className='bg-white lg:mt-16 w-auto lg:h-[85vh]'>
+    <div className='bg-white flex justify-center max-h-fit flex-col items-center lg:justify-start lg:mt-16 w-auto'>
     <motion.h1
         initial={{opacity: 0, x : -35}}
         animate={Starboy}
         ref={ref2}   
-      className='lg:text-5xl text-2xl mt-7 ml-7 lg:ml-[70px] font-bold'>Colabration with top brand's. </motion.h1>
+      className='lg:text-5xl text-lg mt-7 lg:mb-5 md:text-3xl md:mb-8 md:mt-20 font-bold'>Colabration with top brand's. </motion.h1>
       <div className='flex justify-center flex-col lg:flex-row mt-6 gap-10 items-center'>
         <div className='relative'>
-          <img src="https://oqium.com/cdn/shop/files/Screenshot_2023-07-18_at_18.46.24.jpg?v=1689859402&width=430" className='lg:w-[430px]' width={320} alt="" />
+          <img src="https://oqium.com/cdn/shop/files/Screenshot_2023-07-18_at_18.46.24.jpg?v=1689859402&width=430" className='lg:w-[430px]  md:w-[575px]' width={320} alt="" />
           <h1 className="absolute  bottom-48 lg:bottom-60 text-3xl font-bold left-0 right-0 text-white text-center p-2">TOMMY HILFIGER</h1>
         </div>
         <div className='relative'>
-          <img src="https://oqium.com/cdn/shop/files/IMG_2509.jpg?v=1695798933&width=430" className='lg:w-[430px]' width={320} alt="" />
+          <img src="https://oqium.com/cdn/shop/files/IMG_2509.jpg?v=1695798933&width=430" className='lg:w-[430px]  md:w-[575px]' width={320} alt="" />
           <h1 className="absolute text-3xl font-bold bottom-48 lg:bottom-60 left-0 right-0 text-white text-center p-2">ZUDIOVERSE</h1>
         </div>
         <div className='relative'>
-          <img src="https://oqium.com/cdn/shop/files/IMG_2510.jpg?v=1695799132&width=430" className='lg:w-[430px]' width={320} alt="" />
+          <img src="https://oqium.com/cdn/shop/files/IMG_2510.jpg?v=1695799132&width=430" className='lg:w-[430px]  md:w-[575px]' width={320} alt="" />
           <h1 className="absolute  bottom-48 lg:bottom-60 text-2xl font-bold left-0 right-0 text-white text-center p-2">LOUIS VUITTON</h1>
         </div>
       </div>
