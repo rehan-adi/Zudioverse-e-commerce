e5f5240f713471976f0566a55ced78853397889e
Rehan <rehanalire52@gmail.com>
2024-02-09 18:17:01 +0530

    Update on About page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ function About() {
       animate={{opacity: 1, x: 0}}
       transition={{duration: 0.90, delay: 0.50}} 
       className='text-center lg:text-base md:text-2xl md:mt-10 text-sm line mt-6'> <span className='text-black font-semibold'>ZUDIOVERSE</span> now operates over 200 stores in the United States, Canada and Europe, <br className='md:hidden lg:flex'/> offering experiential retail environments and a <br className='md:flex lg:hidden'/> well-curated mix men's product <br className='lg:flex hidden' /> assortments.</motion.p>
-      <div className='flex gap-20 lg:gap-96 flex-col lg:flex-row pb-9 pt-24 items-center'>
+      <div className='flex gap-20 lg:gap-96 flex-col lg:flex-row md:mt-16 lg:mt-7 pb-9 pt-24 items-center'>
         <div className='lg:pl-0 pl-4'>
           <motion.h1
           initial={{opacity: 0, x: -100}}
@@ -43,18 +43,18 @@ function About() {
           initial={{opacity: 0.20, y: -500}}
           animate={{opacity: 1, y: 0, rotatey: 50}}
           transition={{duration: 1.2}} 
-          src="https://assets.website-files.com/61983c3a41be5bad7444a3c0/61f2e70177f8584393e77273_Image020%201.jpg" className='rotate-2 lg:w-[190px] ' width={480} alt="" />
+          src="https://assets.website-files.com/61983c3a41be5bad7444a3c0/61f2e70177f8584393e77273_Image020%201.jpg" className='rotate-2 lg:w-[190px] md:w-[93vw]' width={480} alt="" />
         </div>
       </div>
-      <div className='flex flex-col lg:flex-row gap-16 lg:gap-32 mt-20 lg:mt-8 mb-20 lg:mb-5 items-center'>
+      <div className='flex flex-col lg:flex-row gap-16 lg:gap-32 mt-20 lg:mt-8 md:mb-36 md:mt-20 mb-20 lg:mb-5 items-center'>
         <div>
-          <img src="https://assets.website-files.com/61983c3a41be5bad7444a3c0/61f2e7539d538babf0f423db_Image021%201.jpg" className='' width={480} alt="" />
+          <img src="https://assets.website-files.com/61983c3a41be5bad7444a3c0/61f2e7539d538babf0f423db_Image021%201.jpg" className='md:w-[93vw] lg:w-[480px]' width={480} alt="" />
         </div>
-        <div className='pl-4 lg:pl-0'>
-          <h3 className='mb-5 text-base lg:text-lg opacity-60'>OUR FOUNDER</h3>
-          <h1 className='lg:text-5xl text-4xl mad font-semibold'>BEAUTY <br /> BEHIND <br />
+        <div className='pl-4 md:mt-16 lg:pl-0'>
+          <h3 className='mb-5 text-base lg:text-lg md:text-xl opacity-60'>OUR FOUNDER</h3>
+          <h1 className='lg:text-5xl text-4xl mad font-semibold'>BEAUTY <br className='md:hidden flex'/> BEHIND <br className='md:hidden lg:flex'/>
             THE MADNESS</h1>
-            <p className='lg:text-lg text-base mt-7 '>Rehan started this company in his room <br className='lg:flex hidden' /> apartment. Rabitur cursus ut lectus porta <br className='lg:flex hidden' /> placerat. Vamus quais facilisis nulla id dui <br className='lg:flex hidden' /> ague sellus portor.</p>
+            <p className='lg:text-lg md:text-2xl text-base mt-7 '>Rehan started this company in his room <br className='lg:flex hidden' /> apartment. Rabitur cursus ut lectus porta <br className='lg:flex hidden' /> placerat. Vamus quais facilisis nulla id dui <br className='lg:flex hidden' /> ague sellus portor.</p>
         </div>
       </div>
       <Swiper
@@ -72,22 +72,22 @@ function About() {
         className="mySwiper"
       >
         <SwiperSlide>
-          <h1 className='lg:text-6xl text-xl mb-6 font-semibold'>"SUPERIOR TO OTHER <br /> ATHLETIC WEAR, GOING <br /> TO BUY MORE."</h1>
-          <p className='lg:text-xl text-sm opacity-60'>JENNY HARLEM</p>
+          <h1 className='lg:text-6xl md:text-5xl text-xl mb-6 font-semibold'>"SUPERIOR TO OTHER <br /> ATHLETIC WEAR, GOING <br /> TO BUY MORE."</h1>
+          <p className='lg:text-xl md:text-2xl text-sm opacity-90'>JENNY HARLEM</p>
         </SwiperSlide>
         <SwiperSlide>
-          <h1 className='lg:text-6xl text-xl mb-6 font-semibold'>"YOUR BIKER SHORTS <br /> ARE MY GO TO <br /> RUNNING WEAR EVERY <br /> DAY."</h1>
-          <p className='lg:text-xl text-sm opacity-90'>SARAH CALSEY</p>
+          <h1 className='lg:text-6xl text-xl md:text-5xl mb-6 font-semibold'>"YOUR BIKER SHORTS <br /> ARE MY GO TO <br /> RUNNING WEAR EVERY <br /> DAY."</h1>
+          <p className='lg:text-xl text-sm md:text-2xl opacity-90'>SARAH CALSEY</p>
         </SwiperSlide>
         <SwiperSlide>
-          <h1 className='lg:text-6xl text-xl mb-6 font-semibold'>
+          <h1 className='lg:text-6xl text-xl md:text-5xl mb-6 font-semibold'>
             "I'VE BOUGHT TWO <br /> PAIRS AND NOW I CAN'T <br /> LIVE WITHOUT THEM."
           </h1>
-          <p className='lg:text-xl text-sm opacity-90'>EMILY COLSEN</p>
+          <p className='lg:text-xl text-sm md:text-2xl opacity-90'>EMILY COLSEN</p>
         </SwiperSlide>
         <SwiperSlide>
-          <h1 className='lg:text-6xl text-xl mb-6 font-semibold'>"THE SHORTS WERE <br /> JUST WHAT I NEEDED <br /> TO GET ACTIVE <br /> OUTSIDE."</h1>
-          <p className='lg:text-xl text-sm opacity-90'>TAYLOR FLUTTER</p>
+          <h1 className='lg:text-6xl text-xl md:text-5xl mb-6 font-semibold'>"THE SHORTS WERE <br /> JUST WHAT I NEEDED <br /> TO GET ACTIVE <br /> OUTSIDE."</h1>
+          <p className='lg:text-xl text-sm md:text-2xl opacity-90'>TAYLOR FLUTTER</p>
         </SwiperSlide>
       </Swiper>
     </div>
