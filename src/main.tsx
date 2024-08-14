2d4122139450da3edd3d6295c3f523ac37ce2416
Rehan <rehanalire52@gmail.com>
2024-08-14 20:15:16 +0530

    Add HTMLElement type assertion for ReactDOM.createRoot

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import StoreR from './Components/CartProduct/Store/StoreR.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={StoreR}>
   <BrowserRouter>
     <App />
