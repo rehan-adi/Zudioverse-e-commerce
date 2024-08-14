fc3ab4f8eb3770163fe9a7c0b51cb421b465bef9
Rehan <rehanalire52@gmail.com>
2024-08-14 20:00:59 +0530

    fix some more error

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import StoreR from './Components/CartProduct/Store/StoreR.tsx'
+import StoreR from './Components/CartProduct/Store/StoreR.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={StoreR}>
